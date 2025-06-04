756534995e3d4e1873f2a391e6d7aba032575c4f
veriardiansyah <114470804+veriardiansyah@users.noreply.github.com>
2025-06-04 10:33:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def home():
     return "🚀 Hello from Flask App on Kubernetes with Jenkins!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
